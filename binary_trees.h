cdb82dac293625650762638f474258aa26f24513
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-08 10:55:54 +0000

    task 04

--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
